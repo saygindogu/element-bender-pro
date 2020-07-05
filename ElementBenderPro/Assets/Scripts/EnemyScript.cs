b4eada81e6dca536bc11c90fe71f87b380f97d12
Z. Saygın Doğu <saygindogu@gmail.com>
2020-07-05 18:56:52 +0300

    [New] Files

---
+++ ElementBenderPro/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        print("Ouch, it hurts...");
+    }
+
+
+    private void OnParticleCollision(GameObject other) {
+        print("Hey ho particle collusion");
+    }
+}
